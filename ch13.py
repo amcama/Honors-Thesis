7e9fb09112a00fb1f0b76e55288299e82257346a
Andrea Camarillo <andreacamarillo002@gmail.com>
2023-09-17 13:34:11 -0700

    changes

--- ch13.py
+++ ch13.py
@@ -4,7 +4,12 @@ import numpy as np
 import pandas as pd
 from tqdm.notebook import tqdm
 from sklearn.model_selection import train_test_split
-
+from datasets import Dataset, DatasetDict
+from transformers import AutoTokenizer, AutoConfig, TrainingArguments, Trainer
+from torch import nn
+from transformers.modeling_outputs import SequenceClassifierOutput
+from transformers.models.bert.modeling_bert import BertModel, BertPreTrainedModel
+from sklearn.metrics import accuracy_score
 
 # enable tqdm in pandas
 tqdm.pandas()
@@ -36,8 +41,111 @@ train_df = read_data('sample_training_data/PMC544947-classifictaion-out.json')
 
 print(train_df)
 
-train_df, eval_df = train_test_split(train_df, train_size=0.2)
-# train_df.reset_index(inplace=True, drop=True)
-# eval_df.reset_index(inplace=True, drop=True)
-# print(f'train rows: {len(train_df.index):,}')
-# print(f'eval rows: {len(eval_df.index):,}')
+# --- [4] ---
+train_df, eval_df = train_test_split(train_df, train_size=0.9)
+train_df.reset_index(inplace=True, drop=True)
+eval_df.reset_index(inplace=True, drop=True)
+
+print(f'train rows: {len(train_df.index):,}')
+print(f'eval rows: {len(eval_df.index):,}')
+
+# --- [5] ---
+ds = DatasetDict()
+ds['train'] = Dataset.from_pandas(train_df)
+ds['validation'] = Dataset.from_pandas(eval_df)
+
+# do i need to do tokenizer stuff? json files are already tokenized
+# --- [6] ---
+transformer_name = 'bert-base-cased'
+tokenizer = AutoTokenizer.from_pretrained(transformer_name)
+
+# --- [7] ---
+# def tokenize(examples):
+#     return tokenizer(examples['text'], truncation=True)
+train_ds = ds['train']
+eval_ds = ds['validation']
+train_ds.to_pandas()
+
+
+# --- [8] ---
+class BertForSequenceClassification(BertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+        self.init_weights()
+        
+    def forward(self, input_ids=None, attention_mask=None, token_type_ids=None, labels=None, **kwargs):
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            **kwargs,
+        )
+        cls_outputs = outputs.last_hidden_state[:, 0, :]
+        cls_outputs = self.dropout(cls_outputs)
+        logits = self.classifier(cls_outputs)
+        loss = None
+        if labels is not None:
+            loss_fn = nn.CrossEntropyLoss()
+            loss = loss_fn(logits, labels)
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+# --- [9] ---
+config = AutoConfig.from_pretrained(
+    transformer_name,
+    num_labels=7, # todo make this not hard coded
+)
+
+model = (
+    BertForSequenceClassification.from_pretrained(transformer_name, config=config)
+)
+# print(model)
+
+# --- [10] ---
+num_epochs = 2
+batch_size = 24
+weight_decay = 0.01
+model_name = f'{transformer_name}-sequence-classification'
+
+training_args = TrainingArguments(
+    output_dir=model_name,
+    log_level='error',
+    num_train_epochs=num_epochs,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    evaluation_strategy='epoch',
+    weight_decay=weight_decay,
+    remove_unused_columns=False
+)
+
+# --- [11] ---
+def compute_metrics(eval_pred):
+    y_true = eval_pred.label_ids
+    y_pred = np.argmax(eval_pred.predictions, axis=-1)
+    return {'accuracy': accuracy_score(y_true, y_pred)}
+
+# --- [12] ---
+print()
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    compute_metrics=compute_metrics,
+    train_dataset=train_ds,
+    eval_dataset=eval_ds,
+    tokenizer=tokenizer,
+)
+
+# --- [13] ---
+print()
+trainer.train()
+
+# --- [14] ---
