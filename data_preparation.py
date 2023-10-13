5b4a765ecaca7c6d151c8582ad655afca86350b0
Andrea Camarillo <andreacamarillo002@gmail.com>
2023-10-13 13:27:49 -0700

    Removed duplicates from data

--- data_preparation.py
+++ data_preparation.py
@@ -5,40 +5,50 @@ import pandas as pd
 def main():
     list = []
     directory = 'sample_training_data'
-
     count = 0
+    
     for filename in os.listdir(directory):
         if filename.endswith('.json'):
             with open(os.path.join(directory, filename)) as f:
-                
                 data = json.load(f)
-                # print("data: ", data, "")
                 if (len(data) > 0):
                     list.append(data)
                     count += 1
 
         if (count == 2):
             break
-    no_dups = remove_duplicates(list)
+
+    list_no_dups = remove_duplicates(list)
+    pretty_print(list_no_dups)
 
 
 def remove_duplicates(list):
     # concatenate all nested items into single list
-    list2 = []
+    single_list = []
     for e1 in list:
         for e2 in e1:
-            list2.append(e2)
-            print(e2)
+            single_list.append(e2)
 
+    list_no_duplicates = []
+    seen = set()
 
-            
+    for e in single_list:
+        joined_string = ''.join(e['sentence_tokens'])
+        print(joined_string, "\n")
+        if (joined_string not in seen):
+            list_no_duplicates.append(e)
 
+        seen.add(joined_string)
+
+    # print("-   Seen Count: {}".format(len(single_list)))
+    # print("- Original Count: {}".format(list))
+    # print("-   Unique Count: {}\n".format(len(list_no_duplicates)))
+
+    return list_no_duplicates
+
+    
 def pretty_print(list):
-    for e1 in list:
-        for e2 in e1:
-            print(e2)
-            print()
-    
-    
+    for e in list:
+        print(e, "\n")
 
 main()
